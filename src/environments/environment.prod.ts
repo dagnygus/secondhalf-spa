99e7d318b7ceb8e6143e0e002bbc4f1e7902a01e
Dag Nygard <dagnygard@Dags-MacBook-Pro.local>
2023-03-24 17:02:20 +0100

    commit nr 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { firebase } from "./firebase";
 
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:5001/secondhalf-92707/us-central1/api/',
+  baseUrl: 'https://europe-west3-secondhalf-92707.cloudfunctions.net/api/',
   portaitPlaceholderUrl: '/assets/front_end_images/portrait_placeholder.svg',
   funcrtionsEmulator: true,
   firebase
